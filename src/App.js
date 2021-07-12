b29eede41454357f77371fa1b8ec749e21b2d460
Roxanne <roxanne-m@users.noreply.github.com>
2021-07-12 13:11:48 -0700

    Landing page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Header from './Header/Header';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <main className='App'>
+          <div className='intro'>
+            <p>
+              To begin viewing the latest school data please click the button
+              below.
+            </p>
+            <p>The data that you will be provided with includes:</p>
+            <ul>
+              <li>School Name</li>
+              <li>School Name Alias (if present in data)</li>
+              <li>School Website</li>
+              <li>School City</li>
+              <li>School State</li>
+              <li>School Zipcode</li>
+              <li>The Total Number of Students</li>
+              <li>Data on the School's Program Percentage</li>
+              <li>Data on the School's Race Ethnicity</li>
+            </ul>
+          </div>
+
+          <div>
+            <button>Let's Begin!</button>
+          </div>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Header/Header.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './header.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <h1>Most Recent School Data</h1>
+          <h2>Data.gov</h2>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Header;
